5d231ee117157afc8d7d7d326b86d109f4dd141d
manoj <manojannadurai12345@gmail.com>
2020-10-22 07:40:13 -0700

    add a Comment,js

---
+++ src/Components/Comment/comment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View,StyleSheet,Text,TextInput } from 'react-native';
+
+export default CreateComment = ()=>{
+    <View>
+        <Text>
+            <TextInput>
+                
+            </TextInput>
+        </Text>
+    </View>
+}
+const styles = StyleSheet.create({
+    
+})
