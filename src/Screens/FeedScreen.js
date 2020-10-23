7be20c7f2264ef40b0a71064d0c36aab9403ac0d
sanghavi <sanghavimanian@gmail.com>
2020-10-23 17:18:25 +0530

    Added Wishlist

--- src/Screens/FeedScreen.js
+++ src/Screens/FeedScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView,Image, TouchableOpacity, Alert } from 'react-native';
 import Header from '../Components/Header/Header';
 
 
@@ -14,10 +14,18 @@ export default function FeedScreen () {
             <Text style={{fontSize:20,fontWeight:'400'}}>Amazing Boutique</Text>
             <Text style={{fontSize:16,fontWeight:'700'}}>Location, City</Text></View>
             </View>
-            <View>
+            
+                
+                <ScrollView>
                 <Image source={require('../../assets/dsc.png')}/>
+                <View style={styles.Img1}>
+                <TouchableOpacity onPress={()=> alert('Product added to Wishlist')}>
+                <Image source={require('../../assets/vector.png')}/>
+                </TouchableOpacity>
+                </View>
+                </ScrollView>
             </View>
-        </View>
+        
     )
 }
 
@@ -35,4 +43,8 @@ const styles = StyleSheet.create({
         marginLeft:30,
         marginTop:20,
     },
+    Img1:{
+        marginLeft:365,
+        marginTop:10
+    }
 })
