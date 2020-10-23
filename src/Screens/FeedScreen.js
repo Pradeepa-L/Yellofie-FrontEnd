ee5cab1b533f58181f832f71c9fa96f41c620fa0
athulya <athulyarajesh1285@gmail.com>
2020-10-23 21:45:56 +0530

    add likes to feed

--- src/Screens/FeedScreen.js
+++ src/Screens/FeedScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text,ScrollView,View,Image } from 'react-native';
 import Header from '../Components/Header/Header';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function FeedScreen () {
     return (
@@ -14,15 +14,49 @@ export default function FeedScreen () {
             <Text style={{fontSize:20,fontWeight:'400'}}>Amazing Boutique</Text>
             <Text style={{fontSize:16,fontWeight:'700'}}>Location, City</Text></View>
             </View>
-            <View>
+            <ScrollView><View>
                 <Image source={require('../../assets/dsc.png')}/>
-            </View>
+               
+      </View>
+      <View style={styles.info}> 
+           <Text></Text>
+            <Icon  name="heart" size={30} color="black" style={{ position:"absolute", left:20 }}/>  
+         
+            <Icon name="comment" size={30} color="black"  style={{ position:"absolute", left:60 }} />
+
+            <Icon name="tag" size={30} color="black" style={{ position:"absolute", right:10 }} />
+          
+        </View>
+        <Text></Text>
+        <Text>75011 likes</Text>
+        <Text>amazing_boutique brigtte brianna ruffle skirt</Text>
+      
+            </ScrollView>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+         alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor:"#fff"
+      },
+      imageicon:{
+        width:300,
+         height:350,
+         margin:5,
+       
+      },
+      icons:{
+        width:40,
+        height:40,
+      },
+      
+    
+    
     textview:{
         flexDirection:'row'
     },
@@ -35,4 +69,5 @@ const styles = StyleSheet.create({
         marginLeft:30,
         marginTop:20,
     },
+
 })
